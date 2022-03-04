44b15dc5c91a92c88a3728dc2dd8c5a5c3479dcc
antlogist <antlogist@gmail.com>
2022-03-04 18:24:05 +0300

    Laravel-mix environment added

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+const mix = require("laravel-mix");
+
+mix.sass("resources/assets/sass/app.scss", "dist/css/all.css");
+
+mix.js([
+  "resources/assets/js/app.js"
+], "dist/js/all.js");
+
+mix.minify(['dist/css/all.css']);
+mix.minify(['dist/js/all.js']);
