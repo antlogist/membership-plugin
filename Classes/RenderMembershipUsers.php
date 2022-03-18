51f50fe56fb9343ceb52d709c6b716c960f7df31
antlogist <antlogist@gmail.com>
2022-03-18 16:02:00 +0300

    Modal window markup added

--- Classes/RenderMembershipUsers.php
+++ Classes/RenderMembershipUsers.php
@@ -68,8 +68,53 @@ class RenderMembershipUsers {
                 <button type="button" id="TB_closeWindowButton" @click="openUserMenu(null)" style="width:30px; height:30px; border:1px solid black;">x</button>
               </div>
             </div>
-            <div id="TB_ajaxContent" style="width:600px;height:545px">
-              <p>{{ currentUser.first_name }}</p>
+            <div style="padding-left: 25px; padding-right: 25px;">
+
+              <h1>{{ currentUser.first_name }} {{ currentUser.last_name }}</h1>
+              <div>{{ currentUser.email }}</div>
+
+              <div>
+                <h2>Membership Plan: <span style="font-weight: normal; color: red;">Test Plan</span></h2>
+              </div>
+
+              <div>
+                <h2>Viewable Pages:</h2>
+                <p><input type="checkbox" value="newsletters"> Newsletters</p>
+                <p><input type="checkbox" value="historyPages"> History Pages</p>
+                <button class="button button-primary" style="margin-top: 15px;">Save Changes</button>
+              </div>
+
+              <h2 style="margin-top: 25px;">Orders</h2>
+              <table class="wp-list-table widefat fixed striped table-view-list">
+              <thead>
+                <tr>
+                  <th scope="col" id="usernameTh" class="manage-column column-order-id">Order Id</th>
+                  <th scope="col" id="nameTh" class="manage-column column-product-name">Product</th>
+                  <th scope="col" id="lastNameTh" class="manage-column column-quantity" style="text-align: center;">Quantity</th>
+                  <th scope="col" id="emailTh" class="manage-column column-date" style="text-align: center;">Date</th>
+                </tr>
+              </thead>
+
+              <tbody id="the-list">
+                <tr>
+                  <td class="username-td column-order-id">730</td>
+                  <td class="name-td column-product-name">Test product</td>
+                  <td class="email-td column-quantity text-center" style="text-align: center;">2</td>
+                  <td class="membership-td column-date" style="text-align: center;">2022-03-06</td>
+                </tr>
+              </tbody>
+
+              <tfoot>
+                <tr>
+                <th scope="col" id="usernameTh" class="manage-column column-order-id">Order Id</th>
+                <th scope="col" id="nameTh" class="manage-column column-product-name">Product</th>
+                <th scope="col" id="lastNameTh" class="manage-column column-quantity" style="text-align: center;">Quantity</th>
+                <th scope="col" id="emailTh" class="manage-column column-date" style="text-align: center;">Date</th>
+                </tr>
+              </tfoot>
+
+            </table>
+
             </div>
           </div>
 
